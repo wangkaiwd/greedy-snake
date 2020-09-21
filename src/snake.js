1b12b7a469e3cb0b3674720ea3c4830af8cc9a13
wangkaiwd <1162486176@qq.com>
2020-09-21 14:09:43 +0800

    feat: run snake

--- src/snake.js
+++ src/snake.js
@@ -1,15 +1,38 @@
 function Snake (container) {
   this.container = document.querySelector(container);
+  this.left = 0;
+  this.top = 0;
+  this.timer = null;
+  this.snakeBody = null;
   this.initSnake();
+  this.bindEvent();
 }
 
+Snake.prototype.bindEvent = function () {
+  window.addEventListener('keypress', this.start.bind(this));
+};
+
+Snake.prototype.start = function (e) {
+  if (e.keyCode === 32) {
+    this.run();
+  }
+};
 Snake.prototype.initSnake = function () {
   const snakeBody = document.createElement('div');
-  const snakeBodyItem = document.createElement('div');
   snakeBody.classList.add('snake-body');
-  snakeBodyItem.classList.add('snake-body-item');
-  snakeBody.appendChild(snakeBodyItem);
+  for (let i = 0; i < 2; i++) {
+    const snakeBodyItem = document.createElement('div');
+    snakeBodyItem.classList.add('snake-body-item');
+    snakeBody.appendChild(snakeBodyItem);
+  }
   this.container.appendChild(snakeBody);
+  this.snakeBody = snakeBody;
+};
+Snake.prototype.run = function () {
+  setInterval(() => {
+    this.left += 20;
+    this.snakeBody.style.left = this.left + 'px';
+  }, 400);
 };
 
 export default Snake;
