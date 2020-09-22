143a47508423b39ba9b2e4f6e8f57efd2ee5fd61
wangkaiwd <1162486176@qq.com>
2020-09-22 17:02:13 +0800

    feat: optimize all direction move

--- src/snake.js
+++ src/snake.js
@@ -1,12 +1,19 @@
 import { addClass, createEle } from '@/shared/dom';
 
+const getMoveMap = (child) => {
+  return {
+    37: { direction: 'left', ...child && { value: child.offsetLeft - 20, key: 'left' } },
+    38: { direction: 'up', ...child && { value: child.offsetTop - 20, key: 'top' } },
+    39: { direction: 'right', ...child && { value: child.offsetLeft + 20, key: 'left' } },
+    40: { direction: 'down', ...child && { value: child.offsetTop + 20, key: 'top' } }
+  };
+};
+
 function Snake (container) {
   this.container = document.querySelector(container);
   this.timer = null;
   this.snakeBody = null;
   this.direction = null;
-  this.head = null;
-  this.tail = null;
   this.initSnake();
   this.bindEvent();
 }
@@ -29,27 +36,21 @@ Snake.prototype.initSnake = function () {
   }
   this.container.appendChild(snakeBody);
   this.snakeBody = snakeBody;
-  this.moveToRight();
 };
 
 Snake.prototype.run = function (e) {
-  const map = {
-    37: { direction: 'left', move: this.moveToLeft },
-    38: { direction: 'up', move: this.moveToUp },
-    39: { direction: 'right', move: this.moveToRight },
-    40: { direction: 'down', move: this.moveToDown }
-  };
+  const map = getMoveMap();
   if (!map[e.keyCode]) {return;}
-  const { direction, move } = map[e.keyCode];
-  if (direction === this.direction) {return;}
+  const { direction } = map[e.keyCode];
+  if (isForbiddenChangeDirection(this.direction, direction)) {return;}
+  this.direction = direction;
   clearInterval(this.timer);
   this.timer = setInterval(() => {
-    this.direction = direction;
-    move.call(this);
+    this.move(e.keyCode);
   }, 400);
 };
 
-Snake.prototype.moveToRight = function () {
+Snake.prototype.move = function (keyCode) {
   const children = this.snakeBody.children;
   for (let i = 0; i < children.length; i++) {
     const child = children[i];
@@ -58,52 +59,23 @@ Snake.prototype.moveToRight = function () {
       child.style.left = nextChild.offsetLeft + 'px';
       child.style.top = nextChild.offsetTop + 'px';
     } else { // 蛇头
-      child.style.left = child.offsetLeft + 20 + 'px';
+      const { key, value } = getMoveMap(child)[keyCode];
+      child.style[key] = value + 'px';
     }
   }
 };
 
-Snake.prototype.moveToLeft = function () {
-  const children = this.snakeBody.children;
-  for (let i = 0; i < children.length; i++) {
-    const child = children[i];
-    const nextChild = children[i + 1];
-    if (nextChild) { // 蛇身
-      child.style.left = nextChild.offsetLeft + 'px';
-      child.style.top = nextChild.offsetTop + 'px';
-    } else { // 蛇头
-      child.style.left = child.offsetLeft - 20 + 'px';
-    }
-  }
-};
-
-Snake.prototype.moveToDown = function () {
-  const children = this.snakeBody.children;
-  for (let i = 0; i < children.length; i++) {
-    const child = children[i];
-    const nextChild = children[i + 1];
-    if (nextChild) { // 蛇身
-      child.style.left = nextChild.offsetLeft + 'px';
-      child.style.top = nextChild.offsetTop + 'px';
-    } else { // 蛇头
-      child.style.top = child.offsetTop + 20 + 'px';
-    }
-  }
-};
-
-Snake.prototype.moveToUp = function () {
-  const children = this.snakeBody.children;
-  for (let i = 0; i < children.length; i++) {
-    const child = children[i];
-    const nextChild = children[i + 1];
-    if (nextChild) { // 蛇身
-      child.style.left = nextChild.offsetLeft + 'px';
-      child.style.top = nextChild.offsetTop + 'px';
-    } else { // 蛇头
-      child.style.top = child.offsetTop - 20 + 'px';
-    }
-  }
-};
+function isForbiddenChangeDirection (prevDirection, currentDirection) {
+  return prevDirection === currentDirection
+    ||
+    (prevDirection === 'down' && currentDirection === 'up')
+    ||
+    (prevDirection === 'up' && currentDirection === 'down')
+    ||
+    (prevDirection === 'left' && currentDirection === 'right')
+    ||
+    (prevDirection === 'right' && currentDirection === 'left');
+}
 
 function createEleAndAddClass ({ tag, className }) {
   const ele = createEle(tag);
